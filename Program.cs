aa7e9a45653a78af58a449297997507743f364cb
ls <1354751943@qq.com>
2025-07-16 20:25:27 +0800

    refactor, rename namespcace

--- Program.cs
+++ Program.cs
@@ -3,8 +3,9 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
+using SimpleJson;
 
-namespace AGServer
+namespace AGSyncCS
 {
     class Program
     {
@@ -18,7 +19,12 @@ namespace AGServer
             
             // Start TCP server in a background thread
             Thread tcpServerThread = new Thread(() => {
-                TcpServer tcpServer = new TcpServer(ServerConfig.TCP_SERVER_PORT, ServerConfig.TCP_MAX_CONNECTIONS, ServerConfig.TCP_CONNECTION_TIMEOUT);
+                TcpServer tcpServer = new TcpServer(ServerConfig.TCP_SERVER_PORT, 
+                    ServerConfig.TCP_MAX_CONNECTIONS, ServerConfig.TCP_CONNECTION_TIMEOUT);
+                // Register a listener for path "/test"
+                tcpServer.AddListener("newRoom", node => {
+                    LogService.Instance.Info($"[Listener] Received on newRoom: {node}");
+                });
                 tcpServer.Start();
                 
                 // Keep TCP server running
